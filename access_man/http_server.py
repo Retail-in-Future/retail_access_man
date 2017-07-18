5b182caa7cab782fa422ec541ae4815bb870de72
dgchen <dgchen@thoughtworks.com>
2017-07-18 16:18:01 +0800

    [diangui] add method start server

--- access_man/http_server.py
+++ access_man/http_server.py
@@ -1,18 +1,28 @@
 from eve import Eve
 from eve.auth import BasicAuth
 
-from access_man.utils import Singleton
+# from access_man.utils import Singleton
 
 
 class Auth(BasicAuth):
     def check_auth(self, username, password, allowed_roles, resource, method):
-        pass
+    	return True;
 
+# class RestApiServer(metaclass=Singleton):
+#     def __init__(self):
+#         self.__app = Eve(auth=Auth)
 
-class RestApiServer(metaclass=Singleton):
+#     def start(self):
+#         self.__app.run()
+#     pass
+
+class RestApiServer():
     def __init__(self):
         self.__app = Eve(auth=Auth)
 
     def start(self):
         self.__app.run()
     pass
+if __name__ == '__main__':
+	restApiServer = RestApiServer();
+	restApiServer.start();
